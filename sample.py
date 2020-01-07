68f534c1cad4ee0a1cc5239fe47e88b356a9b2b4
bhavyadeep <bhavyadeep.purswani98@gmail.com>
2020-01-07 20:53:23 +0530

    Improved speed for data loading

---
+++ sample.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan  7 17:34:05 2020
+
+@author: Bhavyadeep Purswani
+"""
+
+import pandas as pd
+df = pd.DataFrame(columns=5)
+df.head()
--- server.py
+++ server.py
@@ -6,14 +6,13 @@ Created on Sat Jan  4 19:33:27 2020
 """
 
 
-from flask import Flask,request,jsonify,make_response,flash,redirect, url_for
+from flask import Flask,request,make_response
 import pandas as pd
-from flask_cors import CORS, cross_origin
+from flask_cors import CORS
 import json
 import os
-from flask import Flask, flash, request, redirect, url_for
 from werkzeug.utils import secure_filename
-import csv
+import numpy as np
 
 #constants
 UPLOAD_FOLDER = '.\data'
@@ -22,12 +21,8 @@ ALLOWED_EXTENSIONS = {'csv', 'xls', 'txt', 'xlsx'}
 #global variables
 global trainFileName
 global trainData
+global trainDataDisplay
 
-#Function to display the data in tabular form 
-def displayData(trainData,testData):
-    displayTrainData=trainData.values.tolist()[0:5]
-    displayTestData=testData.values.tolist()[0:5]
-    return displayTrainData,displayTestData
 
 #Function to check for valid extensions
 def allowed_file(filename):
@@ -39,15 +34,10 @@ def uploadFile():
     responseData=""
     
     if request.method == 'POST':
-        # check if the post request has the file part
         if 'file' not in request.files:
-            #flash('No file part')
             responseData,fileName="No file part",None
         file = request.files['file']
-        # if user does not select file, browser also
-        # submit an empty part without filename
         if file.filename == '':
-            flash('No selected file')
             responseData,fileName="No selected file",None
         if file: 
             if allowed_file(file.filename):
@@ -62,25 +52,20 @@ def uploadFile():
 
 #Load data and convert it into dataframe
 def loadData(filename):
-    fileURL=UPLOAD_FOLDER+"\\"+filename 
-    with open(fileURL, mode='r') as csv_file:
-        csv_reader = csv.reader(csv_file)
-        line_count = 0
-        for row in csv_reader:
-            if line_count==0:
-                df=pd.DataFrame(columns=row)
-            else:
-                df.loc[line_count]=row
-            line_count += 1
+    fileURL=UPLOAD_FOLDER+"\\"+filename
+    if request.form['headerFlag'] == "True":
+        df = pd.read_csv(fileURL)
+    else:
+        df = pd.read_csv(fileURL,header=None)
     return df
 
 #Function to create file head and send it to front end
 def fileHead(df):
+    df.fillna("null",inplace=True)
     displayData=df.values.tolist()[0:5]
     metadata=list(df.columns)
     responseData={"data":displayData,"metaData":metadata}
-    responseData=json.dumps(responseData)
-    #print(displayTrainData)
+    responseData=json.dumps(responseData,ensure_ascii=True,allow_nan=True)
     r = make_response(responseData)
     r.mimetype = 'text/plain'
     return r
@@ -91,15 +76,14 @@ app = Flask(__name__)
 CORS(app)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER 
 
+#Function to return the head of dataframe
 @app.route('/trainHead',methods=['GET'])
 def trainHead():
     global trainData
     r=fileHead(trainData)
     return r   
-    #return jsonify(trainData=displayTrainData,
-                   #testData=displayTestData,
-                   #trainMetaData=trainMetaData)
-                   
+    
+#Function for uploading and loading the file into dataframe               
 @app.route('/trainUpload',methods=['POST'])
 def trainUpload():
     global trainFileName
@@ -113,11 +97,6 @@ def trainUpload():
     r.mimetype = 'text/plain'
     return r
 
-@app.route('/data')
-def data():
-    global trainData
-    print(trainData)
-    
-    
+   
 if __name__ == '__main__':
    app.run()
