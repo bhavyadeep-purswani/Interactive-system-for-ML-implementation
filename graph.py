7ad6b4a641174450d04f34c45f8ee38432a6618f
Lekhana G <lekhanag.3003@gmail.com>
2020-01-14 16:11:56 +0530

    graph intial

---
+++ graph.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 14 12:17:07 2020
+
+@author: lekha
+"""
+from flask import Flask,request
+from flask_cors import CORS
+import pandas as pd
+
+global data
+
+#Flask Code
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/loadGraphData',methods=['POST'])
+def loadGraphData():
+    global data
+    responseData=request.json['graphData']
+    data=pd.DataFrame(responseData)
+    print(data)
+    return "Yes"
+    
+app.run(host='127.0.0.1', port= 5001)
